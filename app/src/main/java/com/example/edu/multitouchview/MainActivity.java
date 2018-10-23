10f76d0134dba0be8fc643b7360c5f7c45b11599
IOT <1po@korea.com>
2018-10-23 13:43:02 +0900

    Initial commit

---
+++ app/src/main/java/com/example/edu/multitouchview/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.edu.multitouchview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(new MultiTouchView(this,null));
+    }
+}
